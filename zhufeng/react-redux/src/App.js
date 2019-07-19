31781dc9f970ab324075900d8318fa509cddf344
MAC_杨德金 <245962518@qq.com>
2019-07-19 21:27:34 +0800

    给store加注释

--- zhufeng/react-redux/src/App.js
+++ zhufeng/react-redux/src/App.js
@@ -3,6 +3,18 @@ import 'antd/dist/antd.css';
 import {Row,Col} from 'antd';
 import './App.css'
 export default class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {num:0};
+
+    }
+    changeClick = ()=>{
+        this.setState({num:this.state.num+1})
+    }
+    componentWillMount(){
+        alert('11')
+        console.log(this.state.num);
+    }
     render(){
         return(
            <div>
@@ -12,6 +24,7 @@ export default class App extends React.Component{
                       <div className="todoHeader">header</div>
                       <div className="todoBody">header</div>
                       <div className="todoFooter">todoFooter</div>
+                      <div><button onClick={this.changeClick}>{this.state.num}</button></div>
                    </div>
                  </Col>
                </Row>
--- zhufeng/react-redux/src/index.js
+++ zhufeng/react-redux/src/index.js
@@ -8,6 +8,7 @@ let store = createStore(todoAPP)
 
 // 每次 state 更新时，打印日志
 // 注意 subscribe() 返回一个函数用来注销监听器
+//当仓库里的state发生变化的时候，会重新执行方法，读取最新的状态数据并更新视图
 const unsubscribe = store.subscribe(() => console.log(store.getState()))
 // 发起一系列 action
 store.dispatch(addTodo('todo新增1'))
