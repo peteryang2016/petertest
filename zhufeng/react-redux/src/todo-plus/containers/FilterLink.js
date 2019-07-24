eb2edda6dd2d1c6c37f16644cc596d947b97efa5
MAC_杨德金 <245962518@qq.com>
2019-07-24 18:02:35 +0800

    容器组件的梳理     footer筛选功能开发

---
+++ zhufeng/react-redux/src/todo-plus/containers/FilterLink.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import Link from '../components/Link'
+import {SET_VISIBILITY_FILTER,} from "../../action";
+import {setVisibilityFilter} from "../actions";
+
+
+
+
+//将redux 中state映射到组件中的state
+let mapStateToProps=(state,ownProps)=>{
+    console.log(state.visibilityFilter);
+    console.log(ownProps.filter);
+    return{
+        active:ownProps.filter===state.visibilityFilter
+    }
+}
+//把展示组件的变化同步到store中
+let mapDispatchToProps=(dispatch,ownProps)=>{
+    return{
+        onClick:()=>dispatch(setVisibilityFilter(ownProps.filter))
+    }
+}
+let FilterLink = connect(mapStateToProps,mapDispatchToProps)(Link)
+export default FilterLink
