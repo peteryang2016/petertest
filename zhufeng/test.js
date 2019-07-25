8a3652cc0c57c5dbae2d8fb957f6de1b6b990104
MAC_杨德金 <245962518@qq.com>
2019-07-25 18:15:43 +0800

    本地删除过的文件同步至git

--- zhufeng/test.js
+++
@@ -1,5 +0,0 @@
-let obj= {path:'etc/abc',component:'Logo',index:'1'}
-let {path,...reset} = obj;
-console.log(path);//etc/abc
-console.log(reset);//{ component: 'Logo', index: '1' }
-console.log({...reset})//{ component: 'Logo', index: '1' }
